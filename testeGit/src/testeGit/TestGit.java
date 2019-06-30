008efa7a6bb771082eb391d4b725bc6aa7259608
diego_sete <diego_sete@yahoo.com.br>
2019-06-30 18:51:13 -0300

    alterei nova brach

--- testeGit/src/testeGit/TestGit.java
+++ testeGit/src/testeGit/TestGit.java
@@ -3,17 +3,6 @@ package testeGit;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//testando git fa
-		//fdafa
-		//vai dar merge
-		int value = 0;
-		
-		int value1 = 0;
-		int value2 = 0;
-		//teste merge
-		//123132132
-
-	}
+		777	}
 
 }
